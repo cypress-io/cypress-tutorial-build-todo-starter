3c66213dd17b9eb9e58ac258c16951882e6560f1
David Svetlecic <DSvetlecic@cylance.com>
2020-09-02 12:47:15 -0500

    2. Text Inputs

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,17 @@
 describe('Input form', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  })
   it('focuses input on load', () => {
-    cy.visit('http://localhost:3030')
+    cy.focused()
+      .should('have.class', 'new-todo')
+  })
+
+  it('accepts input', () => {
+    const typedText = 'Buy Milk' 
+
+    cy.get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText)
   })
 })
